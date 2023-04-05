8591ad5b8d69406c845b0ec1d3ef67e08463b50a
HermanNgwenya <hermanbngwenya@gmail.com>
2023-04-05 14:55:16 +0200

    IWA_07

---
+++ Challenge 1/scripts.js
@@ -0,0 +1,5 @@
+const value = 3
+console.log(value + 4 + value)
+
+/* intially , 343 was logged because of the error in the value of the 'value'
+ variable which was a string instead of a proper number*/
