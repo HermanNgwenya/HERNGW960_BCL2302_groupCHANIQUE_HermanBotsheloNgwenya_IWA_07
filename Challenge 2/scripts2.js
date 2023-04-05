9cc7bca26c52e1c5001f6b5104f75288f19298a5
HermanNgwenya <hermanbngwenya@gmail.com>
2023-04-05 16:01:02 +0200

    IWA_07

--- Challenge 2/scripts2.js
+++ Challenge 2/scripts2.js
@@ -1,4 +1,17 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-console.log("Good Morning, ${nickname} || {firstname}!")
+let result = nickname || firstname
+
+if (!result) {
+    console.log(`Good Morning!`)
+} else {
+    console.log(`Good Morning, ${result}!`)
+}
+
+
+/*
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+console.log("Good Morning, ${nickname} || {firstname}!")*/
---
+++ Challenge 3/scripts3.js
@@ -0,0 +1,22 @@
+const leoName = 'Leo'
+const leoSurname = 'Musvaire     '
+const leoBalance = '-9394'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+
+const divider = '----------------------------------'
+
+// Only change below this line
+
+parseFloat(leoBalance)
+parseFloat(sarahBalance)
+const owed = parseInt('R' + leoBalance + sarahBalance)
+const leo = `${leoName} + ${leoSurname} + \"Owed\" + \"R\" + ${sarahBalance}\n`
+const sarah = `${sarahName} + ${sarahSurname} + \"Owed\" + \"R\" + ${sarahBalance}\n`
+const total = "Total amount owed: "
+
+const result = leo + sarah + divider + divider + total + owed + divider
+
+console.log(result)
